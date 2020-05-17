0a56225f4cf129b4868597cc2115b5ed532c253b
khawa <khavarjk@gmail.com>
2020-05-18 01:16:47 +0500

    remove product detail activity

---
+++ app/src/main/java/sqlite/view/ProductAdapter.java
@@ -0,0 +1,92 @@
+package sqlite.view;
+
+/**
+ * Created by ravi on 20/02/18.
+ */
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import sqlite.R;
+import sqlite.database.model.Product;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.MyViewHolder> {
+
+    private Context context;
+    private List<Product> productList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView productName;
+        public TextView productDescription;
+        public TextView dot;
+        public TextView timestamp;
+
+        public MyViewHolder(View view) {
+            super(view);
+            productName = view.findViewById(R.id.productName);
+            productDescription = view.findViewById(R.id.productDescription);
+            dot = view.findViewById(R.id.dot);
+            timestamp = view.findViewById(R.id.timestamp);
+        }
+    }
+
+
+    public ProductAdapter(Context context, List<Product>productList) {
+        this.context = context;
+        this.productList = productList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.product_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Product product = productList.get(position);
+
+        holder.productName.setText(product.getProductName());
+        holder.productDescription.setText(product.getProductDescription());
+        // Displaying dot from HTML character code
+        holder.dot.setText(Html.fromHtml("&#8226;"));
+
+        // Formatting and displaying timestamp
+        holder.timestamp.setText(formatDate(product.getProductTimestamp()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    /**
+     * Formatting timestamp to `MMM d` format
+     * Input: 2018-02-21 00:15:42
+     * Output: Feb 21
+     */
+    private String formatDate(String dateStr) {
+        try {
+            SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = fmt.parse(dateStr);
+            SimpleDateFormat fmtOut = new SimpleDateFormat("MMM d");
+            return fmtOut.format(date);
+        } catch (ParseException e) {
+
+        }
+
+        return "";
+    }
+}
